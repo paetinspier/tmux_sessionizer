639cb608a4147fa1705a61128e11525d5ecc23aa
Paetin <paetin.nelson@gmail.com>
2024-11-23 15:52:05 -0800

    tmux sessionizer

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/paetinspier/tmux_sessionizer
+
+go 1.23
+
+require github.com/junegunn/fzf v0.56.3
+
+require (
+	github.com/charlievieth/fastwalk v1.0.9 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
+	github.com/go-cmd/cmd v1.4.3
+	github.com/jubnzv/go-tmux v0.0.0-20240808014214-bf465a395e96
+	github.com/junegunn/go-shellwords v0.0.0-20240813092932-a62c48c52e97 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+
+	gotmux "github.com/jubnzv/go-tmux"
+)
+
+func ExecCmd(args []string) error {
+	tmux, err := exec.LookPath("tmux")
+	if err != nil {
+		return err
+	}
+
+	args = append([]string{tmux}, args...)
+
+	if err := syscall.Exec(tmux, args, os.Environ()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ExecCmdWithoutStop(args []string) error {
+	tmux, err := exec.LookPath("tmux")
+	if err != nil {
+		return fmt.Errorf("tmux not found: %w", err)
+	}
+
+	cmd := exec.Command(tmux, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running tmux: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	fzfCmd := "find ~/code ~/.config/ ~/ -mindepth 1 -maxdepth 2 -type d | fzf"
+	cmd := exec.Command("bash", "-c", fzfCmd)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Error running fzf command: %v\n", err)
+	}
+	if len(string(output)) < 1 {
+		os.Exit(0)	
+	}
+	selectedPathSlugs := strings.Split(string(output), "/")
+
+	selectedName := strings.ReplaceAll(strings.Trim(strings.Join(selectedPathSlugs[3:], "/"), "\n"), ".", "_")
+	selectedPath := strings.TrimSpace(strings.Trim(string(output), "\n"))
+
+	server := new(gotmux.Server)
+	sessions, err := server.ListSessions()
+	if err != nil {
+		fmt.Printf("error listing sessions: %v\n", err)
+	}
+
+	if len(sessions) == 0 && !gotmux.IsInsideTmux() {
+		fmt.Println("creating new session")
+		err := ExecCmd([]string{"new-session", "-s", selectedName, "-c", selectedPath})
+		if err != nil {
+			fmt.Printf("Error ExecCmd() new-session with name and path -> %v", err)
+		}
+		os.Exit(0)
+	}
+	hasSession, err := server.HasSession(selectedName)
+	if err != nil {
+		fmt.Printf("error with HasSession: %v", err)
+	}
+
+	if !hasSession && gotmux.IsInsideTmux() {
+		err := ExecCmdWithoutStop([]string{"new-session", "-d", "-s", selectedName, "-c", selectedPath})
+		if err != nil {
+			fmt.Printf("Error ExecCmd() new-session detached with name and path -> %v", err)
+		}
+	}
+	if !hasSession && !gotmux.IsInsideTmux() {
+		err := ExecCmd([]string{"new-session", "-s", selectedName, "-c", selectedPath})
+		if err != nil {
+			fmt.Printf("Error ExecCmd() new-session with name and path -> %v", err)
+		}
+		//os.Exit(0)
+	}
+
+	if !gotmux.IsInsideTmux() {
+		err := ExecCmd([]string{"a", "-t", selectedName})
+		if err != nil {
+			fmt.Printf("Error ExecCmd() attach with name -> %v", err)
+		}
+		//os.Exit(0)
+	}
+
+	err = ExecCmd([]string{"switch-client", "-t", selectedName})
+	if err != nil {
+		fmt.Printf("Error ExecCmd() switch-client with name -> %v", err)
+	}
+	//os.Exit(0)
+
+}
